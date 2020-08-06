c03a179e583e0d680a2bcc119d7e5bc043711980
Beau Barker <beau@barkerest.com>
2020-08-06 17:07:52 -0400

    Add upgrade progress tests.

---
+++ NetOpnApi.Tests/Models/Core/Firmware/UpgradeProgress_Should.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NetOpnApi.Models.Core.Firmware;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NetOpnApi.Tests.Models.Core.Firmware
+{
+    public class UpgradeProgress_Should : BaseModelTest<UpgradeProgress, UpgradeProgress_Should.Params>
+    {
+        public class Params : IEnumerable<ModelTestParam<UpgradeProgress>>
+        {
+            private static readonly IEnumerable<ModelTestParam<UpgradeProgress>> ParamList
+                = new ParamBuilder(@"{""status"":""ok"",""log"":""System is idle.""}")
+                  .AddTestsFor(m => m.Status)
+                  .AddTestsFor(m => m.Log)
+                  .ToArray();
+
+            public IEnumerator<ModelTestParam<UpgradeProgress>> GetEnumerator() => ParamList.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        public UpgradeProgress_Should(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        protected override void Compare(UpgradeProgress expected, UpgradeProgress actual)
+        {
+            Assert.Equal(expected.Status, actual.Status);
+            Assert.Equal(expected.Log, actual.Log);
+        }
+
+        protected override UpgradeProgress Expected => new UpgradeProgress()
+        {
+            Status = "ok",
+            Log    = "System is idle."
+        };
+    }
+}
