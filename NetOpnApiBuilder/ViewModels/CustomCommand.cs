7675fbeb501471d70168e1c07a330f17d3ddd0b3
Beau Barker <bbarker@karasent.com>
2020-09-01 17:08:24 -0400

    Add custom command class.

--- NetOpnApiBuilder/ViewModels/CustomCommand.cs
+++ NetOpnApiBuilder/ViewModels/CustomCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using NetOpnApi;
@@ -10,6 +11,11 @@ namespace NetOpnApiBuilder.ViewModels
 {
     public class CustomCommand : ICommandWithResponseAndParameterSet<JsonElement>
     {
+        private readonly string[]                   _urlKeys;
+        private readonly Dictionary<string, string> _urlParams;
+        private readonly Dictionary<string, string> _queryMap;
+        private readonly Dictionary<string, string> _queryParams;
+
         public CustomCommand(ApiCommand cmd)
         {
             Module     = cmd.Controller.Module.ApiName;
@@ -26,38 +32,71 @@ namespace NetOpnApiBuilder.ViewModels
             {
                 ResponseRootElementValueKind = JsonValueKind.Object;
             }
-            
-            
+
+            ResponseRootElementName = cmd.ResponseBodyPropertyName;
+
+            if (cmd.UrlParams.Any())
+            {
+                _urlKeys   = cmd.UrlParams.Select(x => x.ClrName).ToArray();
+                _urlParams = cmd.UrlParams.ToDictionary(x => x.ClrName, _ => (string) null);
+            }
+            else
+            {
+                _urlKeys   = null;
+                _urlParams = null;
+            }
+
+            if (cmd.QueryParams.Any())
+            {
+                _queryParams = cmd.QueryParams.ToDictionary(x => x.ClrName, _ => (string) null);
+                _queryMap    = cmd.QueryParams.ToDictionary(x => x.ClrName, x => x.ApiName);
+            }
+            else
+            {
+                _queryMap    = null;
+                _queryParams = null;
+            }
         }
-        
+
         public string        Module                       { get; }
         public string        Controller                   { get; }
         public string        Command                      { get; }
         public IDeviceConfig Config                       { get; set; }
         public ILogger       Logger                       { get; set; }
         public bool          UsePost                      { get; }
-        public string        ResponseRootElementName      { get; set; }
+        public string        ResponseRootElementName      { get; }
         public JsonValueKind ResponseRootElementValueKind { get; }
         public JsonElement   Response                     { get; set; }
-        
-        
-        
-        public IReadOnlyList<string>                       GetUrlParameters()
+
+        public IReadOnlyList<string> GetUrlParameters()
         {
-            throw new NotImplementedException();
+            if (_urlKeys is null) return null;
+
+            var ret = new List<string>();
+            foreach (var key in _urlKeys)
+            {
+                var v = _urlParams[key];
+                if (v is null) break;
+                ret.Add(v);
+            }
+
+            return ret;
         }
 
         public IReadOnlyList<KeyValuePair<string, string>> GetQueryParameters()
         {
-            throw new NotImplementedException();
+            return _queryMap
+                   ?.Select(x => new KeyValuePair<string, string>(x.Value, _queryParams[x.Key]))
+                   .Where(x => !(x.Value is null))
+                   .ToArray();
         }
 
-        public object                                      GetRequestPayload()
+        public object GetRequestPayload()
         {
             throw new NotImplementedException();
         }
 
-        public Type                                        GetRequestPayloadDataType()
+        public Type GetRequestPayloadDataType()
         {
             throw new NotImplementedException();
         }
