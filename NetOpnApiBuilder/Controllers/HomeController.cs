7100acf684ad2c42128f38f7e3c16ffbf9dfdb58
Beau Barker <bbarker@karasent.com>
2020-09-08 17:07:52 -0400

    Add ApiExporter class and some information about export prep.

--- NetOpnApiBuilder/Controllers/HomeController.cs
+++ NetOpnApiBuilder/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NetOpnApiBuilder.Models;
 using NetOpnApiBuilder.ViewModels;
 
@@ -9,11 +14,13 @@ namespace NetOpnApiBuilder.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly BuilderDb _db;
+        private readonly BuilderDb      _db;
+        private readonly ILoggerFactory _loggerFactory;
 
-        public HomeController(BuilderDb db)
+        public HomeController(BuilderDb db, ILoggerFactory loggerFactory)
         {
-            _db = db ?? throw new ArgumentNullException(nameof(db));
+            _db            = db ?? throw new ArgumentNullException(nameof(db));
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
         }
 
         public IActionResult Index()
@@ -27,7 +34,7 @@ namespace NetOpnApiBuilder.Controllers
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
 
-        [HttpGet]
+        [HttpGet("device-ready")]
         public async Task<IActionResult> DeviceReady()
         {
             var device = await _db.GetTestDeviceAsync();
@@ -39,6 +46,105 @@ namespace NetOpnApiBuilder.Controllers
                     ready,
                     host       = device.Host,
                     configured = !string.Equals(device.Key, defs.Key) && !string.Equals(device.Secret, defs.Secret)
+                }
+            );
+        }
+
+
+        [HttpGet("export-status")]
+        public async Task<IActionResult> ExportStatus()
+        {
+            var exporter = new ApiExporter(_db, _loggerFactory.CreateLogger<ApiExporter>());
+            await exporter.InitAsync();
+
+            var message = new StringBuilder();
+
+            void AddMessage(int count, string singular, string plural)
+                => message.AppendFormat(count == 1 ? singular : plural, count);
+
+            AddMessage(
+                exporter.Commands.Count,
+                "There is 1 command marked for export.",
+                "There are {0} commands marked for export."
+            );
+
+            if (exporter.CommandsMissingData.Any())
+            {
+                message.Append('\n');
+                AddMessage(
+                    exporter.CommandsMissingData.Count,
+                    "There is 1 command that needs additional data configured in the database.",
+                    "There are {0} commands that need additional data configured in the database."
+                );
+            }
+
+            if (exporter.CommandsMissingTypes.Any())
+            {
+                message.Append('\n');
+                AddMessage(
+                    exporter.CommandsMissingTypes.Count,
+                    "There is 1 command that needs type values selected for post and/or response content.",
+                    "There are {0} commands that need type values selected for post and/or response content."
+                );
+
+            }
+
+            if (exporter.CommandsWithInvalidTypes.Any())
+            {
+                message.Append('\n');
+                AddMessage(
+                    exporter.CommandsWithInvalidTypes.Count,
+                    "There is 1 command that has invalid type values selected for post and/or response content.",
+                    "There are {0} commands that have invalid type values selected for post and/or response content."
+                );
+            }
+
+            if (exporter.Types.Any())
+            {
+                message.Append('\n');
+                AddMessage(
+                    exporter.Types.Count,
+                    "There is 1 type marked for export.",
+                    "There are {0} types marked for export."
+                );
+                if (exporter.TypesWithMissingTypes.Any())
+                {
+                    message.Append('\n');
+                    AddMessage(
+                        exporter.TypesWithMissingTypes.Count,
+                        "There is 1 type that needs type values selected for one or more properties.",
+                        "There are {0} types that need type values selected for one or more properties."
+                    );
+                }
+
+                if (exporter.TypesWithInvalidTypes.Any())
+                {
+                    message.Append('\n');
+                    AddMessage(
+                        exporter.TypesWithInvalidTypes.Count,
+                        "There is 1 type that has invalid type values selected for one or more properties.",
+                        "There are {0} types that have invalid type values selected for one or more properties."
+                    );
+                }
+            }
+
+            if (exporter.Ready)
+            {
+                message.Append("\nReady to export.");
+            }
+
+            return Json(
+                new
+                {
+                    totalCommands            = exporter.Commands.Count,
+                    commandsMissingData      = exporter.CommandsMissingData.Count,
+                    commandsMissingTypes     = exporter.CommandsMissingTypes.Count,
+                    commandsWithInvalidTypes = exporter.CommandsWithInvalidTypes.Count,
+                    totalTypes               = exporter.Types.Count,
+                    typesMissingTypes        = exporter.TypesWithMissingTypes.Count,
+                    typesWithInvalidTypes    = exporter.TypesWithInvalidTypes.Count,
+                    ready                    = exporter.Ready,
+                    message                  = message.ToString()
                 }
             );
         }
---
+++ NetOpnApiBuilder/Models/ApiExporter.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NetOpnApiBuilder.Enums;
+
+namespace NetOpnApiBuilder.Models
+{
+    public class ApiExporter
+    {
+        private readonly BuilderDb _db;
+        private readonly ILogger   _logger;
+        private readonly object    _threadLock   = new object();
+        private          bool      _initializing = false;
+
+        public ApiExporter(BuilderDb db, ILogger<ApiExporter> logger)
+        {
+            _db     = db ?? throw new ArgumentNullException(nameof(db));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        private bool _initialized = false;
+
+
+        public bool                         Initialized              => _initialized;
+        public IReadOnlyList<ApiCommand>    Commands                 { get; private set; }
+        public IReadOnlyList<ApiObjectType> Types                    { get; private set; }
+        public IReadOnlyList<ApiCommand>    CommandsMissingData      { get; private set; }
+        public IReadOnlyList<ApiCommand>    CommandsMissingTypes     { get; private set; }
+        public IReadOnlyList<ApiCommand>    CommandsWithInvalidTypes { get; private set; }
+        public IReadOnlyList<ApiObjectType> TypesWithMissingTypes    { get; private set; }
+        public IReadOnlyList<ApiObjectType> TypesWithInvalidTypes    { get; private set; }
+
+        public bool Ready =>
+            Initialized
+            && Commands.Any()
+            && !CommandsMissingData.Any()
+            && !CommandsMissingTypes.Any()
+            && !CommandsWithInvalidTypes.Any()
+            && !TypesWithMissingTypes.Any()
+            && !TypesWithInvalidTypes.Any();
+
+        public async Task InitAsync()
+        {
+            // ensure initialization only happens once.
+            waitForInitialization:
+            while (_initializing) Thread.Sleep(1);
+
+            lock (_threadLock)
+            {
+                if (_initialized) return;
+                if (_initializing) goto waitForInitialization;
+                _initializing = true;
+            }
+
+            try
+            {
+                Commands = await _db.ApiCommands
+                                    .Include(x => x.Controller)
+                                    .ThenInclude(x => x.Module)
+                                    .ThenInclude(x => x.Source)
+                                    .Where(x => x.Skip == false && x.Controller.Skip == false && x.Controller.Module.Skip == false)
+                                    .ToListAsync();
+
+                _logger.LogInformation($"Found {Commands.Count} commands ready for export.");
+
+                CommandsMissingData = Commands
+                                      .Where(x => x.HasMissingData)
+                                      .ToArray();
+
+                if (CommandsMissingData.Any())
+                {
+                    _logger.LogInformation($"Found {CommandsMissingData.Count} commands missing configuration data.");
+                }
+
+                CommandsMissingTypes = Commands
+                                       .Where(
+                                           x =>
+                                               ((x.ResponseBodyDataType & ApiDataType.Object) == ApiDataType.Object && x.ResponseBodyObjectTypeID is null)
+                                               || ((x.UsePost && (x.PostBodyDataType & ApiDataType.Object) == ApiDataType.Object && x.PostBodyObjectTypeID is null))
+                                       )
+                                       .ToArray();
+
+                if (CommandsMissingTypes.Any())
+                {
+                    _logger.LogInformation($"Found {CommandsMissingTypes} commands missing object type values.");
+                }
+
+                var allTypes = await _db.ApiObjectTypes
+                                        .Include(x => x.Properties)
+                                        .ToListAsync();
+
+                _logger.LogDebug($"Found {allTypes.Count} defined types.");
+
+                CommandsWithInvalidTypes = Commands
+                                           .Where(
+                                               x =>
+                                                   (x.ResponseBodyObjectTypeID != null && !allTypes.Any(y => y.ID == x.ResponseBodyObjectTypeID))
+                                                   || (x.UsePost && x.PostBodyObjectTypeID != null && !allTypes.Any(y => y.ID == x.PostBodyObjectTypeID))
+                                           )
+                                           .ToArray();
+
+                if (CommandsWithInvalidTypes.Any())
+                {
+                    _logger.LogWarning($"Found {CommandsWithInvalidTypes.Count} commands with invalid object type values.");
+                }
+
+                int[] GetUsedTypes(IEnumerable<ApiObjectType> types)
+                    => Commands
+                       // used as a return value.
+                       .Where(x => x.ResponseBodyObjectTypeID.HasValue)
+                       .Select(x => x.ResponseBodyObjectTypeID.Value)
+                       .Union(
+                           Commands
+                               // used as a post body.
+                               .Where(x => x.UsePost && x.PostBodyObjectTypeID.HasValue)
+                               .Select(x => x.PostBodyObjectTypeID.Value)
+                       )
+                       .Union(
+                           types
+                               .SelectMany(x => x.Properties)
+                               // used as a property value from a different type.
+                               .Where(x => x.DataTypeObjectTypeID.HasValue && x.DataTypeObjectTypeID != x.ObjectTypeID)
+                               .Select(x => x.DataTypeObjectTypeID.Value)
+                       )
+                       .Distinct()
+                       .OrderBy(x => x)
+                       .ToArray();
+
+                var usedTypes = GetUsedTypes(allTypes);
+
+                // remove unused types.
+                var removed = allTypes.RemoveAll(x => !usedTypes.Contains(x.ID));
+                while (removed > 0)
+                {
+                    usedTypes = GetUsedTypes(allTypes);
+                    removed   = allTypes.RemoveAll(x => !usedTypes.Contains(x.ID));
+                }
+
+                Types = allTypes.ToArray();
+                _logger.LogInformation($"Found {Types.Count} types to export.");
+
+                TypesWithMissingTypes = Types
+                                        .Where(
+                                            x => x.Properties
+                                                  .Any(y => (y.DataType & ApiDataType.Object) == ApiDataType.Object && y.DataTypeObjectTypeID is null)
+                                        )
+                                        .ToArray();
+
+                if (TypesWithMissingTypes.Any())
+                {
+                    _logger.LogInformation($"Found {TypesWithMissingTypes.Count} types missing object type values.");
+                }
+
+                TypesWithInvalidTypes = Types
+                                        .Where(
+                                            x => x.Properties
+                                                  .Any(y => !Types.Any(z => z.ID == y.DataTypeObjectTypeID))
+                                        )
+                                        .ToArray();
+
+                if (TypesWithInvalidTypes.Any())
+                {
+                    _logger.LogWarning($"Found {TypesWithInvalidTypes.Count} types with invalid object type values.");
+                }
+
+                _initialized = true;
+            }
+            finally
+            {
+                _initializing = false;
+            }
+        }
+        
+        
+    }
+}
