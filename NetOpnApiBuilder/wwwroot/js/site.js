8b54125c1b1f3c1c47093f03a23ba35c4625a77f
Beau Barker <beau@barkerest.com>
2020-09-10 17:25:52 -0400

    Show export ready status.

--- NetOpnApiBuilder/wwwroot/js/site.js
+++ NetOpnApiBuilder/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿
+﻿function escapeHTML(unsafe)
+{
+    return unsafe
+        .replace(/&/g,'&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
+
 $(function() {
     // ------------------------------------------------------- //
     // Multi Level dropdowns
